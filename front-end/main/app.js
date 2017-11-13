fe3c1ea99f7d968a9f02b81a6cdd0c3afefa6bfa
Christiano Contreas <nanomay94@utexas.edu>
2017-11-12 23:29:16 -0700

    added routes to linkes for main view

--- front-end/main/app.js
+++ front-end/main/app.js
@@ -1,6 +1,42 @@
 var $ = require("../../back-end/node_modules/jquery");
 var _ = require("../../back-end/node_modules/underscore");
 var Backbone = require("../../back-end/node_modules/backbone");
+var Router = Backbone.Router.extend({
+	routes: {
+		"": "profile",
+		"profile": "profile",
+		"friends": "friends",
+		"groups": "groups",
+		"messages": "messages",
+		"email": "email",
+		"chat_rooms": "chatRooms",
+		"requests": "requests"
+	}
+});
+
+var router = new Router();
+router.on("route:profile", function() {
+	console.log("will render profile view");
+});
+router.on("route:friends", function() {
+	console.log("will render friends view");
+});
+router.on("route:groups", function() {
+	console.log("will render groups view");
+});
+router.on("route:messages", function() {
+	console.log("will render messages view");
+});
+router.on("route:email", function() {
+	console.log("will render email view");
+});
+router.on("route:chatRooms", function() {
+	console.log("will render chatRooms view");
+});
+router.on("route:requests", function() {
+	console.log("will render requests view");
+});
 
 var controller = require("./controller").default.controller;
 controller.index();
+Backbone.history.start();
