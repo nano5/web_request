2fa7f24bf0b6c7b4a9d2d485ae6beb67227cd134
Christiano Contreas <nanomay94@utexas.edu>
2017-11-15 23:46:57 -0700

    pushing from front end?

--- back-end/server.js
+++ back-end/server.js
@@ -1,34 +1,54 @@
 var express = require('express');
+var expressValidator = require("express-validator");
+var expressSession = require("express-session");
+var MongoStore = require("connect-mongo")(expressSession);
+var bodyParser = require("body-parser")
 var path = require('path');
 var http = require("http");
-//var bodyParser = require('body-parser');
 var webpack = require('webpack');
 var webpackDevMiddleware = require('webpack-dev-middleware');
 
 var app = express();
+
 app.use(express.static(path.join(__dirname, 'bower_components')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
+app.use(expressSession({ cookie: {maxAge: 60000}, secret:"foobar", saveUninitialized: false, resave: false, store: new MongoStore({
+        host: '127.0.0.1',
+        port: '27017',
+        db: 'session',
+        url: 'mongodb://client:password@localhost:27017/web_requestdb'
+    })}));
+//app.use(expressSession({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}));
 var config = require('./webpack.config.js');
 var compiler = webpack(config);
 
-// app.use(express.static(path.join(__dirname, 'bower_components')));
-// app.set('view engine', 'ejs');
-// app.set('views', path.join(__dirname, 'views'));
-// app.use(bodyParser());
-
-// // Tell express to use the webpack-dev-middleware and use the webpack.config.js
-// // configuration file as a base.
+// Tell express to use the webpack-dev-middleware and use the webpack.config.js
+// configuration file as a base.
 app.use(webpackDevMiddleware(compiler, {
   publicPath: '/'
 }));
 
-// app.use(require('./todos.js'));
+// // at this point I need to figure out if I should send the main app or the login app thing
 
-// Serve the files on port 3000.
-app.get("/", function(req, res) {
-	res.sendFile(path.join(__dirname + "/../front-end/main/view.html"));
+app.get("/", function(req, res, next) {
+  var sess = req.session
+  if (sess.views) {
+    sess.views++
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<p>views: ' + sess.views + '</p>')
+    res.write('<p>expires in: ' + (sess.cookie.maxAge / 1000) + 's</p>')
+    res.end()
+  } else {
+    sess.views = 1
+    res.end('welcome to the session demo. refresh!')
+  }
 });
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!\n');
-// });
+
+// // app.get("/", function(req, res) {
+// // 	res.sendFile(path.join(__dirname + "/../front-end/main/view.html"));
+// // });
+
 var httpServer = http.createServer(app);
 httpServer.listen(8080);
--- back-end/webpack.config.js
+++ back-end/webpack.config.js
@@ -22,21 +22,3 @@ module.exports = {
 		path.resolve(__dirname, 'node_modules'), 'bower_components', path.resolve(__dirname,"../front-end")]
 	}
 };
-
-
-
-
-
-// var path = require("path");
-
-// module.exports = {
-// 	entry: "../front-end/main/app.js",
-// 	output: {
-// 		path: path.join(__dirname, "../front-end"),
-// 		filename: "bundle.js"
-// 	},
-// 	resolve: {
-// 		modules: ["bower_components", "node_modules"]
-// 	}
-
-// };
