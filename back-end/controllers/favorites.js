04b886249c7ee34f445d4058310b89b933b048b8
Christiano Contreas <nanomay94@utexas.edu>
2018-01-12 23:54:49 -0700

    ready to start testing remove user from favorites service

--- back-end/controllers/favorites.js
+++ back-end/controllers/favorites.js
@@ -46,17 +46,90 @@ Controller.prototype.post_favorites_add_user = function(req, res) {
 							}
 
 							if (!foundCategory) {
-								console.log("could not find category");
+								console.log("could not find category " + _category);
 								res.sendStatus(404);
 								res.end();
 							}
 						} else {
-							console.log("the user you are trying to add doesn't exist");
+							console.log(_other_username + " does not exist.");
 							res.sendStatus(404);
 							res.end();
 						}
 					}
 				});
+			}
+		});
+	} else {
+		res.sendStatus(404);
+		res.end();
+	}
+}
+
+Controller.prototype.post_favorites_remove_user = function(req, res) {
+	if (req.session.loggedIn === true) {
+		var _username = req.session.username;
+		var _other_username = req.body.other_username;
+		var _category = req.body.category;
+		User.findOne({username: _username}, "favorites_by_category", function(err, user) {
+			if (err) {
+				console.log(err);
+				res.sendStatus(404);
+				res.end();
+			} else {
+				if (user) {
+					User.findOne({username: _other_username}, "_id", function(err, other_user) {
+						if (err) {
+							console.log(err);
+							res.sendStatus(404);
+							res.end();
+						} else {
+							if (other_user) {
+								var foundCategory = false;
+								for (var i = 0; i < user.favorites_by_category; ++i) {
+									if (_category === user.favorites_by_category[i].category) {
+										var index = null;
+										if ((index = idIndexInArray(other_user._id, user.favorites_by_category[i].favorites)) >= 0) {
+											user.favorites_by_category[i].favorites.splice(index, 1);
+											user.save(function(err) {
+												if (err) {
+													console.log(err);
+													res.sendStatus(404);
+													res.end();
+												} else {
+													res.setHeader("Content-Type", "application/json");
+													res.send({});
+													res.end();
+												}
+											});
+										} else {
+											console.log("could not find user in " + user.favorites_by_category[i].category);
+											res.sendStatus(404);
+											res.end();
+										}
+
+										foundCategory = true;
+										break;
+									}
+
+								}
+								if (!foundCategory) {
+									console.log("could not find category " + _category);
+									res.sendStatus(404);
+									res.end();
+								}
+
+
+							} else {
+								console.log(_other_username + " does not exist");
+								res.sendStatus(404);
+								res.end();
+							}
+						}
+					});
+				} else {
+					res.sendStatus(404);
+					res.end();
+				}
 			}
 		});
 	} else {
@@ -104,6 +177,15 @@ function idInArray(id, array) {
 		}
 	}
 	return false;
+}
+
+function inIndexInArray(id, array) {
+	for (var i = 0; i < array.length; ++i) {
+		if (id == array[i]) {
+			return i;
+		}
+	}
+	return -1;
 }
 
 var _controller = new Controller();
